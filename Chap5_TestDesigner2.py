c6a655569affe04cb4b9e02351a74dd3588c5762
SeungSoo Oh <sunnyside74@gmail.com>
2025-11-05 06:59:24 +0900

    챕터5_TestDesigner2.py에 UI 파일을 추가하고 수정한 내용 커밋

--- Chap5_TestDesigner2.py
+++ Chap5_TestDesigner2.py
@@ -1,3 +1,5 @@
+# Line Edit, 
+
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 import sys
@@ -5,13 +7,13 @@ import sys
 # xml --> 객체로 변환
 class ClassMyWindow:
     def __init__(self):
-        self.dlg = loadUi('myqt.ui')
+        self.dlg = loadUi('Chap5_UI.ui')
         self.dlg.pushButton.clicked.connect( self.btnClick ) 		# Event 등록 -> 콘솔에 출력됨
         self.dlg.show()		# dialog 창을 생성함
         
     # Button Click Event Function
     def btnClick(self):
-        print('Button Clicked')
+        self.dlg.lineEdit.setText("헬로 파이썬")
 
 app = QApplication(sys.argv)
 myWindow = ClassMyWindow()
